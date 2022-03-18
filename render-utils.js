d23d38566d120c2e475029106558f959c59435ce
SusanBird <susanbird391@gmail.com>
2022-03-18 09:56:48 -0700

    grabbed DOM for renderGoblin function

---
+++ render-utils.js
@@ -0,0 +1,9 @@
+export function renderGoblin(goblinData) {
+
+    const goblinEl = document.createElement('div');
+    const emojiEl = document.createElement('p');
+    const nameEl = document.createElement('p');
+    const hpEl = document.createElement('p');
+     
+
+}
