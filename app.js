4ba65c3d8aadb69de9d12dc9f05d79c183b0cb67
SusanBird <susanbird391@gmail.com>
2022-03-18 10:44:46 -0700

    app js got DOM and imported function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 // import functions and grab DOM elements
+import { renderGoblin } from './render-utils.js';
+
+const goblinListEl = document.querySelector('.goblins');
+const defeatedNumberEl = document.getElementById('defeated-number');
 
 // let state
 
--- render-utils.js
+++ render-utils.js
@@ -4,6 +4,12 @@ export function renderGoblin(goblinData) {
     const emojiEl = document.createElement('p');
     const nameEl = document.createElement('p');
     const hpEl = document.createElement('p');
-     
 
+    goblinEl.classList.add('goblin');
+
+    nameEl.textContent = goblinData.name;
+    hpEl.textContent = goblinData.hp < 0 ? 0 : goblinData.hp;
+    emojiEl.textContent = goblinData.hp > 0 ? '😈' : '🔥';
+
+    
 }
